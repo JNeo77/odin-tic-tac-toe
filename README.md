27def33fa4ebbdd44e3c6381f02d2bbcad50f6eb
JNeo77 <lifted_2000@yahoo.com>
2024-08-06 21:18:28 -0400

    Complete Game Functionality and HTML Elements
    Complete the display and gameController Functionality
     - Add button to reset the game
     - Add button to play again when game ends
     - Implement game over logic to check for a winner or a draw
     - Display results to the page when the game ends
    
    Update README file

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# odin-tic-tac-toe
+# The Odin Project - JavaScript Course - Tic-Tac-Toe Project Solution
+
+This is a solution to The Odin Project JavaScript Course - Tic-Tac-Toe Project.
+
+## Table of contents
+
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+
+## My process
+
+### Built with
+
+- HTML5
+- CSS (Styles, Flex and Grid for layout)
+- JavaScript
+
+### What I learned
+
+ - Using factory functions and the module pattern (IIFEs) in JavaScript to separate concerns in the app.
+
+### Continued development
+
+### Useful resources
--- scripts/script.js
+++ scripts/script.js
@@ -7,17 +7,17 @@ const gameBoard = (function () {
     for (let i = 0; i < rows; i++) {
       board[i] = [];
       for (let j = 0; j < boxes; j++) {
-        board[i][j] = i > 0 ? board[i-1][j] + 3 : j;
+        board[i][j] = '';
       }
     }
   }
+  
+  setBoard();
 
   const getBoard = () => board;
   const updateBoard = (selectedRow, selectedCol, marker) => {
-    board[selectedRow - 1][selectedCol - 1] = marker;
+    board[selectedRow][selectedCol] = marker;
   };
-  
-  setBoard();
 
   return { getBoard, updateBoard, setBoard };
 })();
@@ -31,60 +31,202 @@ function createPlayer (name, marker) {
   return { getName, getMarker };
 }
 
-function playGame () {
-  const firstName = prompt("What is the first player's name?");
-  const secondName = prompt("What is the second player's name?");
-  const firstPlayer = createPlayer(firstName, 'X');
-  const secondPlayer = createPlayer(secondName, 'O');
+const displayController = (function () {
+  const firstPlayerName = document.querySelector('#player1');
+  const secondPlayerName = document.querySelector('#player2');
+  const playerForm = document.querySelector('#playForm');
+  const boxes = document.querySelectorAll('.box');
+  const messageDisplay = document.querySelector('.messageHeader');
+  const currentBoard = gameBoard.getBoard();
+  const startPlay = document.querySelector('.startPlay');
+  const playAgainBtn = document.querySelector('.playAgainBtn');
+  const restartBtn = document.querySelector('.resetBtn');
+  
+  const getBoxes = () => boxes;
+
+  const turnMessage = (name, marker) => {
+    messageDisplay.textContent = `${name}'s turn!`;
+    messageDisplay.classList.toggle(marker);
+  } 
+
+  const toggleForm = () => {
+    startPlay.classList.toggle('hidden');
+  }
+
+  const togglePlayAgain = () => {
+    playAgainBtn.classList.toggle('hidden');
+  }
+
+  const toggleRestartBtn = () => {
+    restartBtn.classList.toggle('hidden');
+  }
+
+  const resultMessage = (result) => {
+    if (result === 'win') {
+      messageDisplay.textContent = `${gameController.getCurrentPlayer().getName()} wins!`;
+    } else {
+      messageDisplay.textContent = `It's a ${result}!`;
+    }
+  }
+
+  const toggleBoard = () => {
+    boxes.forEach((box) => {
+      box.classList.toggle('hidden');
+    });
+  }
+
+  const renderBoard = () => {
+    boxes.forEach((box) => {
+      if (currentBoard[box.dataset.row][box.dataset.column]) {
+        box.textContent = currentBoard[box.dataset.row][box.dataset.column];
+        box.classList.add(currentBoard[box.dataset.row][box.dataset.column]);
+      }
+    });
+  }
+
+  const resetBoard = () => {
+    boxes.forEach((box) => {
+      if (currentBoard[box.dataset.row][box.dataset.column]) {
+        box.classList.remove(currentBoard[box.dataset.row][box.dataset.column]);
+        box.textContent = '';
+      }
+    });
+  }
+
+  playerForm.addEventListener('submit', (e) => {
+    gameController.initiateGame(firstPlayerName.value, secondPlayerName.value);
+    e.preventDefault();
+    playerForm.reset();
+    toggleForm();
+    toggleRestartBtn();
+  });
+  
+  boxes.forEach((box) => {
+    box.addEventListener('click', () => {
+      const boxRow = box.dataset.row;
+      const boxCol = box.dataset.column;
+      gameController.playRound(boxRow, boxCol);
+    });
+  });
+
+  playAgainBtn.addEventListener('click', () => {
+    messageDisplay.textContent = '';
+    resetBoard();
+    gameBoard.setBoard();
+    toggleBoard();
+    toggleForm();
+    togglePlayAgain();
+  });
+
+  restartBtn.addEventListener('click', () => {
+    gameController.clearPlayers();
+    messageDisplay.textContent = '';
+    resetBoard();
+    gameBoard.setBoard();
+    toggleForm();
+    toggleRestartBtn();
+  });
+
+  return { turnMessage, renderBoard, getBoxes, toggleBoard, resultMessage, togglePlayAgain, toggleRestartBtn };
+})();
+
+const gameController = (function () {
   const board = gameBoard.getBoard();
+  const gameBoxes = displayController.getBoxes();
+  
+  let players = [];
+  let currentPlayer = {};
 
-  const players = [firstPlayer, secondPlayer];
+  const initiateGame = (firstName, secondName) => {
+    const firstPlayer = createPlayer(firstName, 'X');
+    const secondPlayer = createPlayer(secondName, 'O');
+    players.push(firstPlayer, secondPlayer);
+    currentPlayer = players[0];
+    displayTurn();
+  }
 
-  let currentPlayer = players[0];
+  const clearPlayers = () => {
+    currentPlayer = {};
+    players = [];
+  }
+
+  const displayTurn = () => {
+    displayController.turnMessage(currentPlayer.getName(), currentPlayer.getMarker());
+  }
+
+  const getCurrentPlayer = () => currentPlayer;
 
   const switchTurn = () => {
     currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+    displayTurn();
   }
 
-  const isWinner = () => {
-    for (let i = 0; i < 3; i++) {
-      if ( 
-        (board[i][0] === board[i][1] && board[i][1] === board[i][2]) ||
-        (board[0][i] === board[1][i] && board[1][i] === board[2][i])
-      ) { 
-        return true; 
-      } else if (
-        (board[0][0] === board[1][1] && board[1][1] === board[2][2]) ||
-        (board[0][2] === board[1][1] && board[1][1] === board[2][0])
-      ) {
-        return true;
-      }
-     }
-     return false;
-  }
-
-  for (let i = 0; i < 9; i++) {
-    console.log(`${currentPlayer.getName()}'s turn!`);
-    
-    const getRow = parseInt(prompt("Enter row"));
-    const getCol = parseInt(prompt("Enter column"));
-
-    if (typeof board[getRow-1][getCol-1] === "string") {
-      console.log('Space already taken!');
-    } else {
-      gameBoard.updateBoard(getRow, getCol, currentPlayer.getMarker());
-    }
-
-    console.table(board);
-
-    if (isWinner()) { 
-      console.log(`${currentPlayer.getName()} wins!`);
-      break;
-    } else if (i === 8) {
-      console.log("It's a tie!");
-      break;
+  const addMarker = (row, col) => {
+    if (!board[row][col]) {
+      gameBoard.updateBoard(row, col, currentPlayer.getMarker());
+      displayController.renderBoard();
     } else {
       switchTurn();
     }
   }
-}
+
+  const endGame = (result) => {
+    setTimeout(() => {
+      displayController.resultMessage(result);
+      displayController.toggleBoard();
+      displayController.togglePlayAgain();
+      displayController.toggleRestartBtn();
+      clearPlayers();
+    }, 250);
+  }
+  
+  const playRound = (row, col) => {
+    addMarker(row, col);
+    const gameOver = isGameOver(gameBoxes);
+    gameOver ? endGame(gameOver) : switchTurn();
+  }
+
+  const isGameOver = (boxes) => {
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    
+    const boxText = [];
+
+    boxes.forEach((box) => {
+      boxText.push(box.textContent);
+    });
+
+    let winner = '';
+
+    for (let combo of winningCombos) {
+      if (
+        (boxes[combo[0]].textContent === boxes[combo[1]].textContent) 
+          && 
+        (boxes[combo[1]].textContent === boxes[combo[2]].textContent)
+          && 
+        (boxes[combo[0]].textContent !== '')
+      ) {
+        winner = 'win';
+      }
+    }
+    
+    if (winner) {
+      return winner;
+    } else if (boxText.includes('')) {
+      return false;
+    } else {
+      return 'tie';
+    }
+
+  }
+
+  return { initiateGame, playRound, getCurrentPlayer, clearPlayers };
+})();
